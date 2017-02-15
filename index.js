f3c152824f7f111487131101f0a644b85bf684b5
Furkan Doğan <furkandoganktf@gmail.com>
2017-02-15 17:09:50 +0300

    add functions

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ var SKILL_NAME = 'Autism Fact';
  */
 var a = Math.floor(Math.random() * FACTS.length);
 
+
+
+function launchRequest() {
+  console.log('Running LaunchRequest', arguments);
+  this.emit('GetFact');
+}
+
+function getNewFactIntent() {
+  console.log('Running GetNewFactIntent', arguments);
+  this.emit('GetFact');
+}
+
 function getFact() {
   console.log('Running GetFact', arguments);
   var randomFact = FACTS[a];
@@ -24,29 +36,27 @@ function getFact() {
   this.emit(':askWithCard', speechOutput, reprompt, SKILL_NAME, randomFact);
 }
 
+function HelpIntent() {
+  var speechOutput = 'You can say tell me a autism fact, or, you can say exit... What can I help you with?';
+  var reprompt = 'What can I help you with?';
+  this.emit(':ask', speechOutput, reprompt);
+}
+function CancelIntent() {
+  console.log('Running CancelIntent', arguments);
+  this.emit(':tell', 'Otsimo is an education app for children who have autism syndrome. We are creating educational games and parent app. Goodbye!');
+}
+function StopIntent() {
+  console.log('Running StopIntent ', arguments);
+  this.emit(':tell', 'Goodbye!');
+}
+
 var handlers = {
-  LaunchRequest: function () {
-    console.log('Running LaunchRequest', arguments);
-    this.emit('GetFact');
-  },
-  GetNewFactIntent: function () {
-    console.log('Running GetNewFactIntent', arguments);
-    this.emit('GetFact');
-  },
+  LaunchRequest: launchRequest,
+  GetNewFactIntent: getNewFactIntent,
   GetFact: getFact,
-  'AMAZON.HelpIntent': function () {
-    var speechOutput = 'You can say tell me a autism fact, or, you can say exit... What can I help you with?';
-    var reprompt = 'What can I help you with?';
-    this.emit(':ask', speechOutput, reprompt);
-  },
-  'AMAZON.CancelIntent': function () {
-    console.log('Running CancelIntent', arguments);
-    this.emit(':tell', 'Otsimo is an education app for children who have autism syndrome. We are creating educational games and parent app. Goodbye!');
-  },
-  'AMAZON.StopIntent': function () {
-    console.log('Running StopIntent ', arguments);
-    this.emit(':tell', 'Goodbye!');
-  },
+  'AMAZON.HelpIntent': HelpIntent,
+  'AMAZON.CancelIntent': CancelIntent,
+  'AMAZON.StopIntent': StopIntent,
 };
 
 exports.handler = function (event, context, callback) {
