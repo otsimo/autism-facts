36b336737768c052457444abdd91a62c9d624e24
Furkan Doğan <furkandoganktf@gmail.com>
2017-02-15 15:27:16 +0300

    add console arguments

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
-'use strict';
+
 var Alexa = require('alexa-sdk');
 var FACTS = require('./facts');
 
-var APP_ID = "amzn1.echo-sdk-ams.app.[amzn1.ask.skill.1291781f-c9af-4b99-ab3a-34e622ed1c80]";
+var APP_ID = 'amzn1.echo-sdk-ams.app.[amzn1.ask.skill.1291781f-c9af-4b99-ab3a-34e622ed1c80]';
 var SKILL_NAME = 'Autism Fact';
 /**
  * Array containing autism facts.
  */
 var a = Math.floor(Math.random() * FACTS.length);
 
-exports.handler = function (event, context, callback) {
-    console.log("event log"=event);
-    console.log("Running handler function");
-    var alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
-};
-
 function getFact() {
-    console.log("Running GetFact");
-    var randomFact = FACTS[a];
-    a = a + 1;
-    // Create speech output
-    if (typeof randomFact == "undefined") {
-        a = 0;
-        randomFact = FACTS[a];
-        a = a + 1;
-    }
-    var speechOutput = "Here's your fact: " + randomFact + "Do you want more facts?";
-    var reprompt = "Do you want more facts?";
-    this.emit(':askWithCard', speechOutput, reprompt, SKILL_NAME, randomFact);
+  console.log('Running GetFact');
+  var randomFact = FACTS[a];
+  a += 1;
+  // Create speech output
+  if (typeof randomFact === 'undefined') {
+    a = 0;
+    randomFact = FACTS[a];
+    a += 1;
+  }
+  var speechOutput = 'Here\'s your fact: ' + 'randomFact' + 'Do you want more facts?';
+  var reprompt = 'Do you want more facts?';
+  this.emit(':askWithCard', speechOutput, reprompt, SKILL_NAME, randomFact);
 }
 
 var handlers = {
-    'LaunchRequest': function () {
-        console.log("Running LaunchRequest");
-        this.emit('GetFact');
-    },
-    'GetNewFactIntent': function () {
-        console.log("Running GetNewFactIntent");
-        this.emit('GetFact');
-    },
-    'GetFact': getFact,
-    'AMAZON.HelpIntent': function () {
-        var speechOutput = "You can say tell me a autism fact, or, you can say exit... What can I help you with?";
-        var reprompt = "What can I help you with?";
-        this.emit(':ask', speechOutput, reprompt);
-    },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', 'Otsimo is an education app for children who have autism syndrome. We are creating educational games and parent app. Goodbye!');
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', 'Goodbye!');
-    }
+  LaunchRequest: function () {
+    console.log('Running LaunchRequest',arguments);
+    this.emit('GetFact');
+  },
+  GetNewFactIntent: function () {
+    console.log('Running GetNewFactIntent',arguments);
+    this.emit('GetFact');
+  },
+  GetFact: getFact,
+  'AMAZON.HelpIntent': function () {
+    var speechOutput = 'You can say tell me a autism fact, or, you can say exit... What can I help you with?';
+    var reprompt = 'What can I help you with?';
+    this.emit(':ask', speechOutput, reprompt);
+  },
+  'AMAZON.CancelIntent': function () {
+    this.emit(':tell', 'Otsimo is an education app for children who have autism syndrome. We are creating educational games and parent app. Goodbye!');
+  },
+  'AMAZON.StopIntent': function () {
+    this.emit(':tell', 'Goodbye!');
+  },
 };
+
+exports.handler = function (event, context) {
+  console.log('Running handler function',arguments);
+  var alexa = Alexa.handler(event, context);
+  alexa.APP_ID = APP_ID;
+  alexa.registerHandlers(handlers);
+  alexa.execute();
+};
+
