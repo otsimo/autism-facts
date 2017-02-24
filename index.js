8c88aa8181a7345d36ae8f9cb7a72f413010aeba
Furkan Doğan <furkandoganktf@gmail.com>
2017-02-24 14:19:42 +0300

    update speechOutput

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function getFact() {
     randomFact = FACTS[a];
     a += 1;
   }
-  var speechOutput = 'Here\'s your fact: ' + randomFact + 'Do you want more facts?';
+  var speechOutput = 'Here\'s your fact: ' + randomFact + '  Do you want more facts?';
   var reprompt = 'Do you want more facts?';
   this.emit(':askWithCard', speechOutput, reprompt, SKILL_NAME, randomFact);
 }
